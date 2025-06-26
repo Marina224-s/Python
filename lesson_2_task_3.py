2d6e9277ea77e6ecedbdc8f65b7d13b07907c4c1
Marina224-s <ooopogrebok67nina@yandex.ru>
2025-06-26 22:35:46 +0300

    Add files via upload

---
+++ lesson_2_task_3.py
@@ -0,0 +1,15 @@
+import math
+
+def square(side):
+    return math.ceil(side*side)
+
+
+side = float (input("сторона квадрата:"))
+import math
+
+def square(side):
+    return math.ceil(side*side)
+
+
+side = float (input("сторона квадрата:"))
+print (f"площадь квадрата:{(square(side))}")
